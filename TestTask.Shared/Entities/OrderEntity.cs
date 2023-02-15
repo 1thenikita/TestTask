2cf50db6628cc47fda2c1113da93cd943663df3a
Nikita Shidlovksy <nik-801@yandex.ru>
2023-02-15 15:00:42 +0300

    Minore fixes.

--- TestTask.Shared/Entities/OrderEntity.cs
+++ TestTask.Shared/Entities/OrderEntity.cs
@@ -3,13 +3,31 @@ using TestTask.Shared.Models;
 
 namespace TestTask.Shared.Entities;
 
+/// <summary>
+/// Сущность операции.
+/// </summary>
 public class OrderEntity
 {
+    /// <summary>
+    /// ID.
+    /// </summary>
     [Key]
     public Guid ID { get; set; }
+    
+    /// <summary>
+    /// Количество списываемых средств.
+    /// </summary>
     [Required]
     public float Money { get; set; }
+    
+    /// <summary>
+    /// Номер карты для списывания.
+    /// </summary>
     [Required]
     public string CardNumber { get; set; }
+    
+    /// <summary>
+    /// Статус операции.
+    /// </summary>
     public OrderStatus Type { get; set; } = OrderStatus.Processing;
 }
